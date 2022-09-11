4ba84fb1a8b5454e3068bb0ee679ee3fb903fc6e
chacxmolodoy <113280514+chacxmolodoy@users.noreply.github.com>
2022-09-11 21:54:38 +0400

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,30 +1,30 @@
-from collections import defaultdict
 import re
 import pickle
-print("Введите асолютный путь файла для обучения")
-s=input()
-
+from collections import defaultdict
+print("Введите абсолютный путь файла для обучения")
+s = input()
 try:
     with open('model.pkl', 'rb') as file:
-        f=pickle.load(file)
-        sl=defaultdict(dict,f)
+        f = pickle.load(file)
+        sl = defaultdict(dict,f)
 except:
-    sl=defaultdict(dict)
+    sl = defaultdict(dict)
 with open(s, encoding='utf-8') as a:
-    stroka=''
+    stroka = ''
     for c in a:
-        stroka+=c.strip().lower()
+        stroka += c.strip().lower()
     result = re.split(r'\s*[?,!–().;]\s*', stroka)
-    for c in result:
-        g=re.split(r'\s*[,: -]\s*', c)
-        g=list(map(lambda x: x.lower(), g))
-        for j in range(len(g)-1):
-            for i in range(j+1,len(g)):
-                tup=tuple(g[j:i])
-                if g[i] in sl[tup].keys():
-                    sl[tup][g[i]]+=1
+    for i in result:
+        vesss = re.split(r'\s*[,: -]\s*', i)
+        vesss = list(map(lambda x: x.lower(), vesss))
+        for j in range(len(vesss)-1):
+            for i in range(j+1,len(vesss)):
+                tup=tuple(vesss[j:i])
+                if vesss[i] in sl[tup].keys():
+                    sl[tup][vesss[i]] += 1
                 else:
-                    sl[tup][g[i]]=1
+                    sl[tup][vesss[i]] = 1
 with open('model.pkl', 'wb') as basa:
-    sl=dict(sl)
+    sl = dict(sl)
     pickle.dump(sl, basa)
+print(sl)
